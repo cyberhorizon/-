1d5cc2a151e08241a08b3ab4235f63e42d16bb25
WOOJIN LEE <127282094+cyberhorizon@users.noreply.github.com>
2024-05-12 01:04:35 +0900

    Update Castingvalue.java
    빼먹은 코드 추가

--- Castingvalue.java
+++ Castingvalue.java
@@ -22,12 +22,16 @@ public class Castingvalue {
 		System.out.println(var2); //강제 타입 변환 후에 10이 그대로 유지
         
 		long var3=300;
-		long var4= (int) var3;
+		int var4= (int) var3;
 		System.out.println(var3); //강제 타입 변환 이후 300이 그대로 유지
         
 		int var5=65;
 		char var6=(char)var5;
-		System.out.println(var6);
+		System.out.println(var6); //A출력
+
+	        double var7=70;
+		int var8=(int)var7;
+		System.out.println(var7);//70 출력
 		
 		
 	}
